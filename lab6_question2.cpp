662490f1f41ee931f87ea967841c07c67bd9f566
Smrutimedha <31215014+Smrutimedha@users.noreply.github.com>
2017-10-27 16:26:40 +0530

    Update lab6_question2.cpp

--- lab6_question2.cpp
+++ lab6_question2.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
+void que1(int a, int b, int *p){
+b=*p;
+cout<<"a="<<a<<", b="<<b<<", p="<<p<<"\n";
+}
 
-int main() {
-	int a,b;
-	int *p;
-	p=&a;
-	b=*p;
-	cout<<"value of a is: "<<a;
-	cout<<"value of b is: "<<b;
-	cout<<"value of c is: "<<p;
-	return 0;
+void que2(int a, int b, int *p){
+cout<<"a="<<a<<", b="<<b<<", p="<<p<<"\n";
+}
+
+void que3(int a, int b, int *p){
+p=&b;
+cout<<"a="<<a<<", b="<<b<<", p="<<p<<"\n";
+}
+
+int main(){
+int a,b;
+a=3;
+b=7;
+int *p=&a;
+que1(a,b,p);
+que2(2,3,p);
+que3(a,b,p);
+return 0;
 }
